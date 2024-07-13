15f21b815c6e248cd5ba9b5af02d8ef6760278e6
ruhich <ruslanbazrov43@gmail.com>
2024-07-13 15:48:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+using namespace std;
+double findRoot(double (*f)(double),double x0,int n){
+ double x=x0;
+ for(int k=1;k<=n;k++){
+ x=f(x);
+ }
+ return x;
+}
+double f(double x){
+ return 0.5*cos(x);
+}
+double g(double x){
+ return exp(-x);
+}
+double h(double x){
+ return (x*x+6)/5;
+}
+void test(double (*f)(double),double x0,string eq){
+ int n=100;
+ double z;
+ cout<<"Решение уравнения "<<eq<<":\t";
+ z=findRoot(f,x0,n);
+ cout<<z<<endl;
+ cout<<"Проверка найденного решения:\t";
+ cout<<z<<" = "<<f(z)<<endl;
+ for(int k=1;k<=50;k++){
+ cout<<"-";
+ }
+ cout<<endl;
+}
+int main(){
+ system("chcp 1251>nul");
+ test(f,0,"x=0.5cos(x)");
+ test(g,0,"x=exp(-x)");
+ test(h,1,"x=(x*x+6)/5");
+ system("pause>nul");
+ return 0;
+}
